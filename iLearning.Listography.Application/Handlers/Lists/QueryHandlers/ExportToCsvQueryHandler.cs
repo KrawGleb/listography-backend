0904468b63bedd0d896b4974ce4f0e0a7b6f00d4
KrawGleb <krawcevitsch@gmail.com>
2022-10-29 13:01:13 +0300

    Add export to csv

---
+++ iLearning.Listography.Application/Handlers/Lists/QueryHandlers/ExportToCsvQueryHandler.cs
@@ -0,0 +1,91 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using iLearning.Listography.Application.Common.Exceptions;
+using iLearning.Listography.Application.Models.Responses;
+using iLearning.Listography.Application.Requests.Lists.Queries.Export;
+using iLearning.Listography.DataAccess.Interfaces.Repositories;
+using iLearning.Listography.DataAccess.Models.Helpers.Extensions;
+using iLearning.Listography.DataAccess.Models.List;
+using MediatR;
+using Nest;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Globalization;
+using System.Threading;
+
+namespace iLearning.Listography.Application.Handlers.Lists.QueryHandlers;
+
+public class ExportToCsvQueryHandler : IRequestHandler<ExportToCsvQuery, CommonResponse>
+{
+    private readonly IListsRepository _listsRepository;
+
+    public ExportToCsvQueryHandler(IListsRepository listsRepository)
+    {
+        _listsRepository = listsRepository;
+    }
+
+    public async Task<CommonResponse> Handle(ExportToCsvQuery request, CancellationToken cancellationToken)
+    {
+        var fileContent = await GenerateFileContentAsync(request.ListId, cancellationToken);
+
+        return new CommonResponse
+        {
+            Succeeded = true,
+            Body = fileContent
+        };
+    }
+
+    private async Task<string> GenerateFileContentAsync(int listId, CancellationToken cancellationToken)
+    {
+        var list = await GetListAsync(listId, cancellationToken);
+        return GetFileContent(list);
+    }
+
+    private async Task<UserList> GetListAsync(int id, CancellationToken cancellationToken)
+    {
+        return await _listsRepository.GetByIdAsync(options =>
+        {
+            options.Id = id;
+            options.IncludeItems = true;
+            options.IncludeItemTemplate = true;
+        }, cancellationToken)
+            ?? throw new NotFoundException("List not found.");
+    }
+
+    private string GetFileContent(UserList list)
+    {
+        var stringWriter = new StringWriter();
+        TextWriter writer = stringWriter;
+        var csvWriter = new CsvWriter(writer, CultureInfo.InvariantCulture);
+
+        WriteFileContentHeader(csvWriter, list.ItemTemplate!);
+        WriteFileContentBody(csvWriter, list.Items!);
+
+        return stringWriter.ToString();
+    }
+
+    private void WriteFileContentHeader(CsvWriter csvWriter, ListItemTemplate template)
+    {
+        csvWriter.WriteField("Name");
+        foreach (var column in template.CustomFields!)
+        {
+            csvWriter.WriteField(column.Name);
+        }
+        csvWriter.NextRecord();
+    }
+
+    private void WriteFileContentBody(CsvWriter csvWriter, IEnumerable<ListItem> items)
+    {
+        foreach (var item in items)
+        {
+            csvWriter.WriteField(item.Name);
+
+            foreach (var customField in item.CustomFields!)
+            {
+                csvWriter.WriteField(customField.GetDisplayedValue());
+            }
+
+            csvWriter.NextRecord();
+        }
+    }
+}
---
+++ iLearning.Listography.Application/Requests/Lists/Queries/Export/ExportToCsvQuery.cs
@@ -0,0 +1,9 @@
+﻿using iLearning.Listography.Application.Models.Responses;
+using MediatR;
+
+namespace iLearning.Listography.Application.Requests.Lists.Queries.Export;
+
+public class ExportToCsvQuery : IRequest<CommonResponse>
+{
+    public int ListId { get; set; }
+}
